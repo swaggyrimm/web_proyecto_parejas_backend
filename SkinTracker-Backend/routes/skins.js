99bd6534ef54046deed1b4a190cbc4704093d61a
swaggyrimm <jorim.jwe30@gmail.com>
2022-07-09 23:22:35 -0400

    I'm dumb

--- SkinTracker-Backend/routes/skins.js
+++ SkinTracker-Backend/routes/skins.js
@@ -9,8 +9,8 @@ const {
 const router = express.Router();
 
 // setting the different routes for the requests
-//router.route("/").get(filterSearch);
-router.route("/").get(getChampSkinsByName);//arreglar esta ruta
+//router.route("/").get(filterSearch); hay que descomentar para que filtre el home y etc...
+router.route("/").get(getChampSkinsByName);//arreglar esta ruta para poder llamarla diferente de / para que no choque con la de filterSearch
 router.route("/:id").get(getSkinById);
 
 
